47c7bcacbc7359ebd97feb4db2293b8cb4885939
Andrew <andrewjrohrer@gmail.com>
2017-09-18 16:44:31 -0500

    Update verbage of registration page
    Update verbage of registration page

--- webroot/JS/scripts.js
+++ webroot/JS/scripts.js
@@ -1,4 +1,4 @@
-function loadLoginPage() {
+function loadRegistrationPage() {
     
     var actvtyspc = document.getElementById("activitySpace");
     actvtyspc.innerHTML='<object style="height:100%; width:100%" type="text/html" data="register.html" ></object>';
