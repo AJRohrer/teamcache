22af53e83fb7faad3ccac8c3bfe5c6b89c7042ca
ULINEDM\andrew.rohrer <andrew.rohrer@uline.com>
2017-09-21 13:22:58 -0500

    Update folder structure

--- webroot/JS/scripts.js
+++
@@ -1,5 +0,0 @@
-function loadRegistrationPage() {
-    
-    var actvtyspc = document.getElementById("activitySpace");
-    actvtyspc.innerHTML='<object style="height:100%; width:100%" type="text/html" data="register.html" ></object>';
-}
