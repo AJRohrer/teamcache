2239cb85b106b31a571ab35a137ad173f06f1fe2
TOOTHLESS\Sebastian <sebbykurp@gmail.com>
2017-11-19 23:29:07 -0600

    Added search just need the accordion button to work.

--- webroot/MainPagev3/JS/mainpage.js
+++ webroot/MainPagev3/JS/mainpage.js
@@ -81,23 +81,19 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
 }
 
-/*Nav-Bar username, accordion*/
+function accordion(){
 var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    }
+  acc[i].onclick = function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  }
 }
-
+}
