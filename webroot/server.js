f30b7d2af1814144519a6505998c2b4023efc8db
AJRohrer <andrewjrohrer@gmail.com>
2017-11-28 21:44:55 -0600

    Add server.js file, make some edits to main page to fix search bar. Added gitignore.

--- webroot/server.js
+++ webroot/server.js
@@ -1,17 +1,39 @@
-const express = require('express');
-const app = express();
+var express = require("express");
+var app = express();
+var port = 3000;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
-app.set('port', 3000);
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost:27017/node-test",{ useMongoClient: true });
+var nameSchema = new mongoose.Schema({
+    proname: {type:String},
+    detailmain: {type:String},
+    // details: [Schema.Types.Mixed]
+},{strict: false});
 
-app.use(express.static(path.join(__dirname,'MainPagev3/index.html')));
+var User = mongoose.model("Project", nameSchema);
 
-var server = app.listen(app.get('port'), function(){
-    var port = server.address().port;
-    console.log('Magic happens on port' + port);
+app.use(express.static(__dirname + '/MainPagev3'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/MainPagev3/mainpage.html");
 });
 
-// express.static(webroot)
+app.post("/addproject", (req, res) => {
+    var myData = new User(req.body);
+    myData.save()
+        .then(item => {
+            res.send("Project saved to database");
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+});
 
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => {
+    console.log("Server listening on port " + port);
+});
