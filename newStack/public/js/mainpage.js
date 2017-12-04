de3fb27817b66659d1b20dc23dc2eacfa1ce53dd
AJRohrer <andrewjrohrer@gmail.com>
2017-12-03 21:43:36 -0600

    Start populating tiles with data from database.

--- newStack/public/js/mainpage.js
+++ newStack/public/js/mainpage.js
@@ -1,81 +1,74 @@
 
-$(document).ready(function() {
+var projects = {};
+$(document).ready(function () {
     $.ajax({
         type: 'POST',
         url: 'http://localhost:8000/getallprojects',
         contentType: 'application/json',
-        success: function(projects){
+        success: function (projects) {
             console.log(JSON.stringify(projects));
+
+            const template = $('#project_template').html()
+            const container = $('#activitySpace');
+
+            for (let i = 0; i < projects.length; i++) {
+                // for (let j = 0; j < projects.steps.length; j++){
+                //     console.log(projects[i].steps[j]);
+                // }
+                container.append(Mustache.render(template, projects[i]))
+            }
+
+
         }
     })
 });
 
-
-function toggleFavorite(myelement){
-    if (myelement.style.color != "red"){
+function toggleFavorite(myelement) {
+    if (myelement.style.color != "red") {
         myelement.style.color = "red";
         //add item to user favorites here once connected to db.
     }
-    else{
+    else {
         myelement.style.color = "white";
         //remove item from user favorites here once connected to db.
     }
 }
 
 
-function displayFileUpload(){
-    document.getElementById("file").setAttribute("style","visibility: visible");
+function displayFileUpload() {
+    document.getElementById("file").setAttribute("style", "visibility: visible");
 }
 
 var fileClose = document.getElementById("file-close");
-fileClose.onclick = function(){
-    document.getElementById("file").setAttribute("style","visibility: hidden");
+fileClose.onclick = function () {
+    document.getElementById("file").setAttribute("style", "visibility: hidden");
 }
 
-document.getElementById("fileName").onchange = function(evt){
-    var target = evt.target || window.event.srcElement, files =target.files;
+document.getElementById("fileName").onchange = function (evt) {
+    var target = evt.target || window.event.srcElement, files = target.files;
 
-    if(FileReader && files && files.length){
+    if (FileReader && files && files.length) {
         var reader = new FileReader();
-        reader.onload = function(){
+        reader.onload = function () {
             document.getElementById("profileImg").src = reader.result;
         }
         reader.readAsDataURL(files[0]);
-        document.getElementById("file").setAttribute("style","visibility: hidden");
-    }else{
-        alert("File not supported"); 
+        document.getElementById("file").setAttribute("style", "visibility: hidden");
+    } else {
+        alert("File not supported");
     }
 }
-var view = {
-    Title: "View Stars",
-    Step1: "Get a telescope",
-    Step2: "Set a viewing angle",
-    Step3: "Look through the lense",
-    Video: "../media/video/stars.mp4",
-    Picture: "../media/images/stars.jpg"
-};
-
-function addItem(container, template) {
-    container.append(Mustache.render(template, view));
-};
-  
-/*This runs on start of the website.*/
-$(() => {
-    const tmpl = $('#project_template').html()
-    const container = $('#activitySpace');
-    for(let i=0; i<9; i++) { addItem(container, tmpl); }
-});
 
 //Get project data from a json format. This will eventually call the database that will return
 //json data.
 function getProjects() {
-    var $deferredNotesRequest = $.getJSON("../data/testprojects.json", {format: "json"});
+    var $deferredNotesRequest = $.getJSON("../data/testprojects.json", { format: "json" });
 }
 
 //when getProjects request is done render each of the project tiles.
-$.when(getProjects()).done(function(response){
+$.when(getProjects()).done(function (response) {
     var $projects = response.projectProperties //projectProperties is the name in the json file.
-    $projects.forEach(function(item){
+    $projects.forEach(function (item) {
         createProject(item.project);
     });
 });
